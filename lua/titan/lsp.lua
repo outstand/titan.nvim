c165c1fb2dc72bcb45b1056dc88bd68e9a59fbf2
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-08-10 14:10:37 +0000

    Replace LSP config with mason-style

--- lua/titan/lsp.lua
+++ lua/titan/lsp.lua
@@ -1,22 +1,7 @@
 local M = {}
 
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.setup{
-  ensure_installed = {
-    'clangd',
-    'rust_analyzer',
-    'pyright',
-    'terraformls',
-    'bashls',
-    'elixirls',
-    'solargraph',
-    'tsserver',
-    'sumneko_lua',
-  },
-}
-
+local mason_lspconfig = require "mason-lspconfig"
 local lspconfig = require 'lspconfig'
-local lsputil = require("lspconfig.util")
 
 local util = require("titan.util")
 
@@ -54,216 +39,157 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilities)
 
---- Build the language server command.
--- @param opts options
--- @param opts.name string Server name
--- @param opts.locations string[] Locations to search relative to the workspace root
--- @param opts.fallback_locations string[] Fallback locations to search relative to
---        nvim-lsp-installer root path or specified fallback_root
--- @param opts.fallback_root? string User customized fallback root. Defaults to
---        ~/.local/share/lsp
--- @return a string containing the command
-local function language_server_cmd(opts)
-  opts = opts or {}
+local function lsp_setup(server_name, opts)
+  logger.debug("Adding", server_name, "to lspconfig")
+  lspconfig[server_name].setup(opts)
+end
 
-  local locations = opts.locations or {}
-  local fallback_locations = opts.fallback_locations or {}
-  local fallback_root = opts.fallback_root or "~/.local/share/lsp"
+local function build_command(server_name, path, args)
+  args = args or {}
 
-  local root = util.workspace_root()
-  if not root then
-    root = vim.loop.cwd()
-  end
-
-  for _, location in ipairs(locations) do
-    local exists, dir = util.dir_has_file(root, location)
-    if exists then
-      logger.fmt_debug("language_server_cmd: %s", vim.fn.expand(dir))
-      return vim.fn.expand(dir)
-    end
-  end
-
-  local root_dir
-
-  local server_is_found, server = lsp_installer.get_server(opts.name)
-  if server_is_found and server:is_installed() then
-    root_dir = server.root_dir
+  local exists, dir = util.workspace_has_file(path)
+  logger.debug("workspace_has_file", exists, dir)
+  if exists then
+    dir = vim.fn.expand(dir)
+    logger.fmt_debug("%s: %s %s", server_name, dir, args)
+    return vim.list_extend({ dir }, args)
   else
-    root_dir = fallback_root
+    return nil
+  end
+end
+
+local function lsp_cmd_override(server_name, opts, cmd_path, args)
+  args = args or {}
+
+  local cmd = build_command(server_name, cmd_path, args)
+  if cmd ~= nil then
+    opts.cmd = cmd
   end
 
-  for _, location in ipairs(fallback_locations) do
-    local exists, dir = util.dir_has_file(root_dir, location)
-    if exists then
-      logger.fmt_debug("language_server_cmd: %s", vim.fn.expand(dir))
-      return vim.fn.expand(dir)
-    end
-  end
-
-  local fallback = vim.fn.expand(lsputil.path.join(fallback_root, fallback_locations[#fallback_locations]))
-  logger.fmt_debug("language_server_cmd: %s", fallback)
-  return fallback
-end
-
---- Build the elixir-ls command.
--- @param opts options
--- @param opts.fallback_dir string Path to use if locations don't contain the binary
-local function elixirls_cmd(opts)
-  opts = opts or {}
-  opts = vim.tbl_deep_extend(
-    "force",
-    opts,
-    {
-      name = "elixirls",
-      locations = {
-        ".elixir-ls-release/language_server.sh",
-        ".elixir_ls/release/language_server.sh",
-      },
-      fallback_locations = {
-        "elixir-ls/language_server.sh",
-      },
-    }
-  )
-
-  return language_server_cmd(opts)
-end
-
---- Build the solargraph command.
--- @param opts options
--- @param opts.fallback_dir string Path to use if locations don't contain the binary
-local function solargraph_cmd(opts)
-  opts = opts or {}
-  opts = vim.tbl_deep_extend(
-    "force",
-    opts,
-    {
-      name = "solargraph",
-      locations = {
-        ".bin/solargraph",
-      },
-      fallback_locations = {
-        "bin/solargraph",
-        "solargraph",
-      },
-    }
-  )
-
-  return language_server_cmd(opts)
-end
-
-local function tsserver_cmd(opts)
-  opts = opts or {}
-  opts = vim.tbl_deep_extend(
-    "force",
-    opts,
-    {
-      name = "tsserver",
-      locations = {
-        ".bin/typescript-language-server",
-      },
-      fallback_locations = {
-        "node_modules/.bin/typescript-language-server",
-        "typescript-language-server",
-      },
-    }
-  )
-
-  return language_server_cmd(opts)
-end
-
-M.custom_commands = nil
-
-function M.add_custom_command(name, cmd)
-  M.custom_commands[name] = cmd
-end
-
-function M.reload_custom_commands()
-  logger.debug("Reload custom commands: ", M.custom_commands)
-  for name, cmd in pairs(M.custom_commands) do
-    logger.debug("Reloading ", name, cmd)
-
-    local config = lspconfig[name]
-
-    if config.manager then
-      for _, client in ipairs(config.manager.clients()) do
-        logger.debug("Stopping client", client.name)
-        client.stop(true)
-      end
+  opts.on_new_config = function(new_config, _)
+    local new_cmd = build_command(server_name, cmd_path, args)
+    if new_cmd ~= nil then
+      new_config.cmd = new_cmd
     end
   end
 end
 
 function M.setup()
-  M.custom_commands = {}
+  require("mason").setup()
+  mason_lspconfig.setup {
+    ensure_installed = {
+      'clangd',
+      'rust_analyzer',
+      'pyright',
+      'terraformls',
+      'bashls',
+      'elixirls',
+      'solargraph',
+      'tsserver',
+      'sumneko_lua',
+    },
+  }
 
   -- vim.lsp.set_log_level("trace")
   -- require("vim.lsp.log").set_format_func(vim.inspect)
 
-  M.add_custom_command("elixirls", function()
-    return { elixirls_cmd() }
-  end)
-  M.add_custom_command("solargraph", function()
-    return { solargraph_cmd(), "stdio" }
-  end)
-  M.add_custom_command("tsserver", function()
-    return { tsserver_cmd(), "--stdio" }
-  end)
+  mason_lspconfig.setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function (server_name) -- default handler (optional)
+      local opts = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
 
-  local enhance_server_opts = {
-    ["elixirls"] = function(opts)
-      opts.cmd = M.custom_commands.elixirls()
-      opts.on_new_config = function(new_config, _)
-        new_config.cmd = M.custom_commands.elixirls()
-      end
+      lsp_setup(server_name, opts)
+    end,
+
+    ["elixirls"] = function(server_name)
+      local opts = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+
+      lsp_cmd_override(
+        server_name,
+        opts,
+        ".elixir-ls-release/language_server.sh"
+      )
+
       opts.settings = {
         elixirLS = {
           mixEnv = "test",
         }
       }
+
+      lsp_setup(server_name, opts)
     end,
-    ["solargraph"] = function(opts)
-      opts.cmd = M.custom_commands.solargraph()
-      opts.on_new_config = function(new_config, _)
-        new_config.cmd = M.custom_commands.solargraph()
-      end
+
+    ["solargraph"] = function(server_name)
+      local opts = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+
+      lsp_cmd_override(
+        server_name,
+        opts,
+        ".bin/solargraph",
+        { "stdio" }
+      )
+
       opts.settings = {
         solargraph = {
           folding = false,
           logLevel = "debug",
         }
       }
+
+      lsp_setup(server_name, opts)
     end,
-    ["tsserver"] = function(opts)
-      opts.cmd = M.custom_commands.tsserver()
-      opts.on_new_config = function(new_config, _)
-        new_config.cmd = M.custom_commands.tsserver()
-      end
+
+    ["tsserver"] = function(server_name)
+      local opts = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+
+      lsp_cmd_override(
+        server_name,
+        opts,
+        ".bin/typescript-language-server",
+        { "--stdio" }
+      )
+
       opts.init_options = {
         hostInfo = "neovim",
         logVerbosity = "verbose"
       }
+
+      lsp_setup(server_name, opts)
     end,
-    ["sumneko_lua"] = function(opts)
-      local runtime_path = vim.split(package.path, ';')
-      table.insert(runtime_path, 'lua/?.lua')
-      table.insert(runtime_path, 'lua/?/init.lua')
+
+    ["sumneko_lua"] = function(server_name)
+      local opts = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
 
       opts.settings = {
         Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
             version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
           },
           diagnostics = {
             -- Get the language server to recognize the `vim` global
-            globals = { 'vim' },
+            globals = {'vim'},
           },
           workspace = {
             -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file('', true),
-            useGitIgnore = false,
+            library = vim.api.nvim_get_runtime_file("", true),
           },
           -- Do not send telemetry data containing a randomized but unique identifier
           telemetry = {
@@ -271,21 +197,10 @@ function M.setup()
           },
         },
       }
+
+      lsp_setup(server_name, opts)
     end,
   }
-
-  for _, server in ipairs(lsp_installer.get_installed_servers()) do
-    local opts = {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-
-    if enhance_server_opts[server.name] then
-      enhance_server_opts[server.name](opts)
-    end
-
-    lspconfig[server.name].setup(opts)
-  end
 
   -- luasnip setup
   local luasnip = require 'luasnip'
